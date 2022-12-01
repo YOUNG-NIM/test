4c71a88adbc2c2e939f27826c6e110673007a53a
YOUNG <ay57105710@gmail.com>
2022-12-01 16:20:48 +0900

    열여덟번째 에러체크

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,15 +16,19 @@ export default NextAuth({
                 password: { label: "비밀번호", type: "password" }
             },
             async authorize(credentials, req) {
-
-                if ( credentials?.email === "testuser@email.com" && credentials.password === "test" ) {
-
-                    const user = { id: 1, name: "test user", email: "testuser@example.com" }
-                    return user;
-
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+          
+                if (user) {
+                  // Any object returned will be saved in `user` property of the JWT
+                  return user
                 } else {
-                    return null
+                  // If you return null then an error will be displayed advising the user to check their details.
+                  return null
+          
+                  // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
                 }
+
             }
         })
     ],
